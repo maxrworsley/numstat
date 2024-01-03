573bd7fd415d5f4e3b77d677c95dd556c16e721f
Max Worsley <maxworsley@outlook.com>
2024-01-03 13:27:41 +0000

    Add fastapi interface using rust function

---
+++ src/num_calc/num_stat/main.py
@@ -0,0 +1,14 @@
+from num_calc import get_stats
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class StatNumber(BaseModel):
+    number: int
+
+app = FastAPI()
+
+@app.post("/stats")
+def root(number: StatNumber):
+    results = get_stats(number)
+    return results
--- src/num_calc/num_stat/serve.py
+++
@@ -1,3 +0,0 @@
-from num_calc import get_stats
-
-print(get_stats(2))
